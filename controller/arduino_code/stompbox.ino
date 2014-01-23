748839ef7208fa7e469abfeada10617add7b9958
Robotto <mark@moore.dk>
2014-01-23 10:52:29 +0100

    fixed fatal mistake where i had put the HTML containers of files in these dirs, instead of the files... so much for archiving stuff on my blog :/

--- controller/arduino_code/stompbox.ino
+++ controller/arduino_code/stompbox.ino
@@ -1,4 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
-<html><head><title>moore.dk</title></head><frameset rows="100%"><frame src="http://mark.moore.dk/stompbox/stompbox.ino">
-<noframes><p>Your user agent does not support frames or is currently configured not to display frames. However
-you may visit <a href="http://mark.moore.dk/stompbox/stompbox.ino">the page that was supposed to be here</a></p></noframes></frameset></html>
+
+
+#define NUM_INPUTS 3
+#define DEBOUNCE_MS 15
+
+typedef struct {
+  byte pinNumber;
+  long debounce_counter;
+  boolean last_state;
+  char key_binding;
+  }
+Input;
+
+Input tree;   //KEY_LEFT_ARROW
+Input rocket; //KEY_UP_ARROW
+Input dragon; //KEY_RIGHT_ARROW
+
+Input *input_map[NUM_INPUTS]={&tree,&rocket,&dragon}; //map of inputs. (array of pointers)
+
+long last_millis = 0;
+
+
+
+void setup()
+{
+
+ tree.pinNumber=13;
+ rocket.pinNumber=12;
+ dragon.pinNumber=15;
+
+ tree.key_binding =     '1';
+ rocket.key_binding =   '2';
+ dragon.key_binding =   '3';
+
+
+for (int i = 0; i < NUM_INPUTS; i++)
+  {
+    input_map[i]->debounce_counter=0;
+    pinMode(input_map[i]->pinNumber,INPUT_PULLUP);
+  }
+
+  Keyboard.begin();
+
+  last_millis = millis();
+}
+
+
+void loop()
+{
+
+
+  if(millis() != last_millis) //If a millisecond has passed
+  {
+    for (int i = 0; i < NUM_INPUTS; i++)
+    {
+      if(digitalRead(input_map[i]->pinNumber)!=input_map[i]->last_state) input_map[i]->debounce_counter++; //if input has changed
+      else  input_map[i]->debounce_counter=0;
+
+      if(input_map[i]->debounce_counter>=DEBOUNCE_MS)
+      {
+        input_map[i]->last_state=digitalRead(input_map[i]->pinNumber);
+        Keyboard.write(input_map[i]->key_binding);
+        input_map[i]->debounce_counter=0;
+      }
+    }
+
+    last_millis = millis();
+  }
+}
